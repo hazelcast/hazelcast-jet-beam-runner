d4d0a1da394a45bcdb50f643211e721d91de5133
Viliam Durina <viliam-durina@users.noreply.github.com>
2019-03-15 09:56:57 +0100

    Fix CreateViewProcessor

--- src/main/java/com/hazelcast/jet/beam/processors/CreateViewProcessor.java
+++ src/main/java/com/hazelcast/jet/beam/processors/CreateViewProcessor.java
@@ -2,28 +2,27 @@ package com.hazelcast.jet.beam.processors;
 
 import com.hazelcast.jet.beam.SideInputValue;
 import com.hazelcast.jet.core.AbstractProcessor;
+import com.hazelcast.jet.core.ResettableSingletonTraverser;
 import org.apache.beam.sdk.util.WindowedValue;
 import org.apache.beam.sdk.values.PCollectionView;
 
 import java.util.Collections;
-import java.util.concurrent.TimeUnit;
 
 public class CreateViewProcessor extends AbstractProcessor {
 
-    private final PCollectionView view;
+    private final ResettableSingletonTraverser<SideInputValue> traverser = new ResettableSingletonTraverser<>();
+    private final FlatMapper<Object, SideInputValue> flatMapper;
 
     public CreateViewProcessor(PCollectionView view) {
-        this.view = view;
+        flatMapper = flatMapper(item -> {
+            traverser.accept(new SideInputValue(view, WindowedValue.valueInGlobalWindow(
+                    Collections.singleton(((WindowedValue) item).getValue()))));
+            return traverser;
+        });
     }
 
     @Override
-    protected boolean tryProcess(int ordinal, Object item) throws Exception {
-        WindowedValue windowedValue = (WindowedValue) item;
-        SideInputValue sideInputValue = new SideInputValue(view, WindowedValue.valueInGlobalWindow(Collections.singleton(windowedValue.getValue())));
-
-        while (!tryEmit(sideInputValue)) { //todo: how to do better, ie. without garbage?
-            TimeUnit.MILLISECONDS.sleep(10);
-        }
-        return true;
+    protected boolean tryProcess(int ordinal, Object item) {
+        return flatMapper.tryProcess(item);
     }
 }
